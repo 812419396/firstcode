05b98fa1b82046e955772cc03536eabc2c9e281a
Z <812419396@qq.com>
2017-02-17 20:53:53 +0800

    注册失败

--- 登录/conn.php
+++ 登录/conn.php
@@ -2,8 +2,19 @@
 /**
 *Promgram:数据库连接包含文件
 *Date:2017-02-13
-*@author:L
+*@author:Z
 *@version:1.0
 **/
+
+$conn=@mysql_connect("localhost","root","");
+if(!$conn){
+	die("数据库连接失败：".mysql_error());
+}
+mysql_select_db("first_code",$conn);
+
+//字符转换，读库
+mysql_query("set character set 'gbk' ");
+//写库
+mysql_query("set names 'gbk'");
 
 ?>
